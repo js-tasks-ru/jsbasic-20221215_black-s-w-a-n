5b36940b00b76a8f2d5052bf86b4fcfd6c954d75
DovgiyA <black.s.w.a.n@yandex.ru>
2022-12-21 15:02:36 +0000

    truncate 2

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,6 +1,6 @@
 function truncate(str, maxlength) {
-   if (str.length > maxlength) {
-    return console.log(`${str.slice(0, maxlength)}…`);
+  if (str.length > maxlength) {
+    return `${str.slice(0, maxlength)}…`;
   }
-  return console.log(str);
+  return str;
 }
