0076826cf63795f2fc547ddaa4f946cd20aae36f
DovgiyA <black.s.w.a.n@yandex.ru>
2022-12-21 15:09:29 +0000

    truncate 4

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,6 +1,6 @@
 function truncate(str, maxlength) {
   if (str.length >= maxlength) {
-    return `${str.slice(0, maxlength)}…`;
+    return `${str.slice(0, maxlength + 1)}…`;
   }
   return str;
 }
