b2585a0be6f12ca99d67087a3902e4f9e223decd
DovgiyA <black.s.w.a.n@yandex.ru>
2022-12-22 05:57:55 +0000

    checkSpam 3

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,5 +1,5 @@
 function checkSpam(str) {
-  if (str.includes('1XbeT') || str.includes('xxx')) {
+  if (str.includes('1XbeT') || str.includes('1xBet') || str.includes('xxx') || str.includes('XXX')) {
     return true;
   }
   return false;
