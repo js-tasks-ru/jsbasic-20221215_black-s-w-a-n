64223cf0c9af41aa67add6c608acf3aeade7e01b
Alex <black.s.w.a.n@yandex.ru>
2023-01-06 14:20:57 +0300

    makeDiagonalRed2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,7 +1,6 @@
 function makeDiagonalRed(table) {
   const arr = table.querySelectorAll('tr');
-  for (let i = 0; i <= arr.length - 1; i++) {
+  for (let i = 0; i < arr.length; i++) {
     table.rows[i].cells[i].style.backgroundColor = 'red';
   }
-  return table;
 }
