5a085eadda797eaeda2afaa4051aeb2022da5b46
Alex <black.s.w.a.n@yandex.ru>
2023-01-12 22:28:48 +0300

    initCarousel2

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,31 +1,33 @@
 function initCarousel() {
   let offset = 0;
   let offsetWidth = document.querySelector('.carousel__img').offsetWidth;
-  document.querySelector('.carousel__arrow_left img').style.display = 'none';
+  const leftArrow = document.querySelector('.carousel__arrow_left img');
+  const rightArrow = document.querySelector('.carousel__arrow_right img');
   const arrows = document.querySelector('.carousel');
   const slide = document.querySelector('.carousel__inner');
+  leftArrow.style.display = 'none';
 
   arrows.addEventListener('click', (event) => {
     
-    if (event.target.getAttribute('src') === "/assets/images/icons/angle-icon.svg") {
+    if (event.target === rightArrow) {
       offset -= offsetWidth;
       slide.style.transform = `translateX(${offset}px)`;
 
       if (offset <= (-offsetWidth * 3)) {
         event.target.style.display = 'none';
       } else {
-        document.querySelector('.carousel__arrow_left img').style.display = '';
+        leftArrow.style.display = '';
       }
     }
 
-    if (event.target.getAttribute('src') === "/assets/images/icons/angle-left-icon.svg") {
+    if (event.target === leftArrow) {
       offset += offsetWidth;
       slide.style.transform = `translateX(${offset}px)`;
     
       if (offset >= 0) {
         event.target.style.display = 'none';
       } else {
-        document.querySelector('.carousel__arrow_right img').style.display = '';
+        rightArrow.style.display = '';
       }
     } 
   });
