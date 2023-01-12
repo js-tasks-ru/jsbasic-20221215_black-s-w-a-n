354c39002a3e993adc11893c8729947f538b5b1f
Alex <black.s.w.a.n@yandex.ru>
2023-01-12 21:55:08 +0300

    initCarousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,32 @@
 function initCarousel() {
-  // ваш код...
+  let offset = 0;
+  let offsetWidth = document.querySelector('.carousel__img').offsetWidth;
+  document.querySelector('.carousel__arrow_left img').style.display = 'none';
+  const arrows = document.querySelector('.carousel');
+  const slide = document.querySelector('.carousel__inner');
+
+  arrows.addEventListener('click', (event) => {
+    
+    if (event.target.getAttribute('src') === "/assets/images/icons/angle-icon.svg") {
+      offset -= offsetWidth;
+      slide.style.transform = `translateX(${offset}px)`;
+
+      if (offset <= (-offsetWidth * 3)) {
+        event.target.style.display = 'none';
+      } else {
+        document.querySelector('.carousel__arrow_left img').style.display = '';
+      }
+    }
+
+    if (event.target.getAttribute('src') === "/assets/images/icons/angle-left-icon.svg") {
+      offset += offsetWidth;
+      slide.style.transform = `translateX(${offset}px)`;
+    
+      if (offset >= 0) {
+        event.target.style.display = 'none';
+      } else {
+        document.querySelector('.carousel__arrow_right img').style.display = '';
+      }
+    } 
+  });
 }
