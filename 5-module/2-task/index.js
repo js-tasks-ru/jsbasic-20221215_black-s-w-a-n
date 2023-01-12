bfc6a9d97dbbaba1c79b1cb564a31e386c406094
Alex <black.s.w.a.n@yandex.ru>
2023-01-12 15:24:24 +0300

    toggleText

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector('.toggle-text-button');
+  button.addEventListener('click', () => {
+    const Div = document.querySelector('#text');
+    if (!Div.hidden) {
+      return Div.hidden = true;
+    }
+    Div.hidden = false;
+  });
 }
